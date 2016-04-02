d9f021d498fb64217ee892403b748eb4bfb06223
Michael Lobanov <miyu8@mail.ru>
2016-04-02 03:46:35 +0300

    Task06 version 4

---
+++ EventExample/Model/Person.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Errors;
+
+namespace EventExample.Model
+{
+    public class Person
+    {
+        public string Name { get; private set; }
+        public DateTime Time;
+        public Person(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                throw new PersonError();
+            Name = name;
+        }
+        public void OnIncomming(Person person)
+        {
+            if (Time.Hour < 12)
+            {
+                Console.WriteLine("'[Доброе утро, {0}]', - сказал {1}", person.Name, this.Name);
+            }
+            else if (Time.Hour < 17)
+            {
+                Console.WriteLine("'[Добрый день, {0}]', - сказал {1}", person.Name, this.Name);
+            }
+            else
+            {
+                Console.WriteLine("'[Добрый вечер, {0}]', - сказал {1}", person.Name, this.Name);
+            }
+        }
+        public void OnLeaving(Person person)
+        {
+            Console.WriteLine("'[До свидания, {0}]', - сказал {1}", person.Name, this.Name);
+        }
+
+    }
+
+}
---
+++ EventExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using EventExample.Interfaces;
+using EventExample.Model;
+using Errors;
+
+namespace EventExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                IScheduler scheduler = new Scheduler();
+                IOffice office = new Office();
+                var monitor = new Monitor();
+                office.IncommingEvent += monitor.OnIncomming;
+                office.LeavingEvent += monitor.OnLeaving;
+                scheduler.SchedulerAction += office.OnSchedulerAction;
+                scheduler.StartEmulation();
+            }
+            catch (PersonError)
+            {
+                Console.WriteLine("Имя человека не может быть не заполнено");
+            }
+            Console.ReadKey();
+        }
+    }
+}
